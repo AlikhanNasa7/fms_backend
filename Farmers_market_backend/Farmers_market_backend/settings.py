426849156364da98dc1d53d72b6413028cd069c9
AlikhanNasa7 <alihannashtaj@gmail.com>
2024-11-29 21:58:46 +0500

    Alikhan: tokens

--- Farmers_market_backend/Farmers_market_backend/settings.py
+++ Farmers_market_backend/Farmers_market_backend/settings.py
@@ -1,5 +1,9 @@
 import pymysql
 from datetime import timedelta
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 pymysql.install_as_MySQLdb()
 
@@ -161,3 +165,23 @@ SPECTACULAR_SETTINGS = {
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
 }
+
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_REGION_NAME = os.getenv('AWS_REGION_NAME')
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_REGION_NAME}.amazonaws.com/'
+
+
+AWS_S3_FILE_OVERWRITE = False 
+AWS_DEFAULT_ACL = None 
+
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
