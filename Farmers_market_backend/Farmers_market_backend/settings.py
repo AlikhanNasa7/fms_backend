09f68dc1b8273a03c8a4ff7b7f52559f921321ce
AlikhanNasa7 <alihannashtaj@gmail.com>
2024-11-30 21:52:11 +0500

    1234

--- Farmers_market_backend/Farmers_market_backend/settings.py
+++ Farmers_market_backend/Farmers_market_backend/settings.py
@@ -1,10 +1,12 @@
 import pymysql
 from datetime import timedelta
-
+import os
 pymysql.install_as_MySQLdb()
-
+from dotenv import load_dotenv
 from pathlib import Path
 
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
