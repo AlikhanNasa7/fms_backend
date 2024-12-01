95fcac7af5e5e69670322fd7f1e61044efc516aa
AlikhanNasa7 <alihannashtaj@gmail.com>
2024-12-01 18:45:39 +0500

    filters

--- Farmers_market_backend/Farmers_market_backend/settings.py
+++ Farmers_market_backend/Farmers_market_backend/settings.py
@@ -191,7 +191,3 @@ MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_REGION_NAME}.amazonaws.c
 AWS_S3_FILE_OVERWRITE = False 
 AWS_DEFAULT_ACL = None 
 
-
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
