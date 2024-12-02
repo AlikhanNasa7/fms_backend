726fe8a4039134ec260f81d6eea09f7c43c2bcc0
Edige-prog <akimali.edige@gmail.com>
2024-12-02 16:29:19 +0500

    email notification for order

--- Farmers_market_backend/orders/api/views.py
+++ Farmers_market_backend/orders/api/views.py
@@ -15,6 +15,8 @@ from orders.models import Order, OrderItem
 from products.models import Product
 from users.models import Buyer
 from rest_framework import status
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 class OrderViewset(viewsets.GenericViewSet):
@@ -32,10 +34,12 @@ class OrderViewset(viewsets.GenericViewSet):
         )
 
         items = CartItem.objects.filter(cart_id=cart)
+        products = []
         for item in items:
             print(item.product_id)
             farm_id = get_object_or_404(Farm, pk=item.farm_id_id)
             product = get_object_or_404(Product, pk=item.product_id_id)
+            products.append((product.name, item.quantity))
             OrderItem.objects.create(
                 order_id=order,
                 product_id=product,
@@ -43,6 +47,14 @@ class OrderViewset(viewsets.GenericViewSet):
                 farm_id=farm_id,
             )
             item.delete()
+
+        send_mail(
+            'You have successfully created an order',
+            f'Dear user, your order id:{order.order_id }. You have ordered {products} Thank you!',
+            settings.DEFAULT_FROM_EMAIL,
+            [buyer.user.email],
+            fail_silently=False,
+        )
 
 
         return Response({"message": "Successfully created an order"}, status=status.HTTP_201_CREATED)
